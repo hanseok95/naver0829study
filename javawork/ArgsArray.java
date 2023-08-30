060125c26291ab882a54f3a3eeb8c4f9604d5302
unknown <hanseok95@naver.com>
2023-08-30 11:48:05 +0900

    2일차

---
+++ javawork/ArgsArray.java
@@ -0,0 +1,20 @@
+public class ArgsArray {
+public static void main(String[]args)
+{
+    //args 로 두개의 숫자를 읽어서 출력해보자
+	String str1=args[0];
+	String str2=args[1];
+	System.out.println("str1="+str1);
+	System.out.println("str2="+str2);
+	System.out.println("두수의합="+(str1+str2));
+
+
+}
+
+
+
+
+
+
+
+}
