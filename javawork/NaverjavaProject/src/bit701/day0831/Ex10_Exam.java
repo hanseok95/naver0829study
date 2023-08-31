d7cd16414781181db90199909613c2c0ac3e8afa
unknown <hanseok95@naver.com>
2023-08-31 20:00:43 +0900

    변수선언

---
+++ javawork/NaverjavaProject/src/bit701/day0831/Ex10_Exam.java
@@ -0,0 +1,40 @@
+package bit701.day0831;
+
+import java.util.Scanner;
+
+public class Ex10_Exam {
+
+	public static void main(String[] args) {
+		TODO Auto-generated method stub
+		Scanner hs=new Scanner(System .in);
+		System.out.println("당신의 나이를 입력하세요");
+	int age=hs. nextInt();
+		System.out.println("응!! 나는 "+age+"세야!!");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//Scanner sc=new Scanner(System.in);
+		//System.out.println("당신의 나이를 입력하세요");
+		//int age=sc.nextInt();
+		//System.out.println("응!! 나는 "+age+"세야!!");
+		
+	}
+
+}
