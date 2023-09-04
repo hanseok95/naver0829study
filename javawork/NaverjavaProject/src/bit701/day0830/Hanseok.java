a33c4d4cd9cf05af3c0365fe684c5885755f3a44
unknown <hanseok95@naver.com>
2023-09-04 12:34:01 +0900

    0904 수업

--- javawork/NaverjavaProject/src/bit701/day0830/Hanseok.java
+++ javawork/NaverjavaProject/src/bit701/day0830/Hanseok.java
@@ -1,9 +1,24 @@
 package bit701.day0830;
 
-public class Hanseok {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Hanseok{
 
+	public static void main(String[] args) { 
 		
+	// TODO Auto-generated method stub
+		Scanner cs=new Scanner(System .in);
+		int age=cs. nextInt();
+		
+		System.out.println("당신의 나이를 입력하세요");
+		System.out.println("응!! 나는 "+age+"세야!!");
+		
+		
+				
+
+
+ }
+
+
+
 }
---
+++ javawork/NaverjavaProject/src/bit701/day0904/Ex2_While.java
@@ -0,0 +1,5 @@
+package bit701.day0904;
+
+public class Ex2_While {
+
+}
