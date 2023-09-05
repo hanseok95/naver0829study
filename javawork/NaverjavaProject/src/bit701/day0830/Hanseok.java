5ada97b9f6ddc947a7e2e71055f3e9c0c5c8b97c
unknown <hanseok95@naver.com>
2023-09-05 14:07:15 +0900

    0905

--- javawork/NaverjavaProject/src/bit701/day0830/Hanseok.java
+++ javawork/NaverjavaProject/src/bit701/day0830/Hanseok.java
@@ -2,23 +2,72 @@ package bit701.day0830;
 
 import java.util.Scanner;
 
-public class Hanseok{
+public class hanseok {
 
-	public static void main(String[] args) { 
-		
-	// TODO Auto-generated method stub
-		Scanner cs=new Scanner(System .in);
-		int age=cs. nextInt();
-		
-		System.out.println("당신의 나이를 입력하세요");
-		System.out.println("응!! 나는 "+age+"세야!!");
-		
-		
-				
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+
+		//				for (int i = 1; i <= 5; i++) {
+		//					for (int j = 1; j <= i; j++) {
+		//						if (i == j){
+		//							System.out.print(i);
+		//						}else{
+		//							System.out.print("*");
+		//					}
+		//					System.out.println();
+		//				}
+		//			}
+		//		
+
+		//		Scanner sc=new Scanner(System.in);
+		//		int money=0  , total= 0, num;
+		//
+		//		Exit:
+		//			while(true){
+		//				System.out.println("=".repeat(40));
+		//				System.out.println("1. 예금 2. 출금 3. 잔고 4.종료");
+		//				System.out.println("=".repeat(40));
+		//				System.out.println("선택>");
+		//				num = sc.nextInt();
+		//				switch(num)
+		//				{ 
+		//				case 1 : 
+		//					System.out.println("예금액:");
+		//					money =sc.nextInt();
+		//					total += money;
+		//					break;
+		//
+		//				case 2 : 
+		//					System.out.println("출금액:");
+		//					money =sc.nextInt();
+		//					total -= money;
+		//					break;
+		//
+		//				case 3 :
+		//					System.out.println("잔고액:"+ total);					
+		//					break;
+		//
+		//				case 4:
+		//					break Exit;
+		//
+		//				default: 
+		//					System.out.println("1-4로 입력바람");
+		//
+		//				}
+		//
+		//			}
+		//		System.out.println("**종료**");
+
+		//if문으로 바꾸기
+		//			Scanner sc=new Scanner(System.in);
+		//			int money=0  , total= 0, num;
+		//			
+		//			if() {						}
+		//			
 
 
- }
+		//158//
+		Scanner sc=new Scanner(System.in);
+		String ssn ="0000000"
+		while(true) {
 
-
-
-}
